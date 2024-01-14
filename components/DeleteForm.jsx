9b7be78b34eb123d799599809af4b5299a529465
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-01-14 19:23:35 +0100

    adding_form_valudation

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,13 +1,43 @@
-import { deleteTask } from '@/utils/actions'
-import React from 'react'
+"use client";
+import { deleteTask } from "@/utils/actions";
+import React, { useEffect } from "react";
+
+import { useFormStatus, useFormState } from "react-dom";
+import toast from "react-hot-toast";
+
+const DeleteBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      <button type="submit" className="btn btn-xs btn-error" disabled={pending}>
+        {pending ? "deleting..." : "delete task"}
+      </button>
+    </>
+  );
+};
+
+const initialState = {
+  message: null,
+};
 
 const DeleteForm = ({ id }) => {
-  return (
-    <form action={deleteTask}>
-        <input type="hidden" name="id" value={id} />
-        <button className="btn btn-xs btn-error">delete</button>
-    </form>
-  )
-}
+  const [deleteState, deleteFormAction] = useFormState(deleteTask, initialState);
 
-export default DeleteForm
+  useEffect(() => {
+    if(deleteState.message === "error"){
+      toast.error("there was an error");
+      return;
+    }if(deleteState.message === "success"){
+      toast.success("Successfuly deleted");
+      return;
+    }
+  }, [deleteState]);
+  return (
+    <form action={deleteFormAction}>
+      <input type="hidden" name="id" value={id} />
+      <DeleteBtn />
+    </form>
+  );
+};
+
+export default DeleteForm;
--- utils/actions.js
+++ utils/actions.js
@@ -2,7 +2,7 @@
 import { revalidatePath } from "next/cache";
 import prisma from "./db";
 import { redirect } from "next/navigation";
-
+import { z } from "zod";
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -22,37 +22,65 @@ export const createTask = async (formData) => {
   revalidatePath("/tasks");
 };
 
-export const deleteTask = async (formData) => {
-  const id = formData.get("id");
-
-  await prisma.task.delete({
-    where: { id },
+export const createTaskCustom = async (prevState, formData) => {
+  // await new Promise(resolve => setTimeout(resolve, 2000))
+  const content = formData.get("content");
+  const Task = z.object({
+    content: z.string().min(5),
   });
-  revalidatePath("/tasks");
+  try {
+    Task.parse({ content });
+    await prisma.task.create({
+      data: {
+        content,
+      },
+    });
+    revalidatePath("/tasks");
+    return { message: "success" };
+  } catch (error) {
+    console.log(error);
+    return { message: "error" };
+  }
+};
+
+export const deleteTask = async (prevState, formData) => {
+  try {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    const id = formData.get("id");
+  
+    await prisma.task.delete({
+      where: { id },
+    });
+    revalidatePath("/tasks");
+    return { message: "success" };
+  } catch (error) {
+    console.log(error);
+    return { message: "error" };
+  }
+
 };
 
 export const getSingleTask = async (id) => {
   return await prisma.task.findFirst({
     where: {
-      id
+      id,
     },
   });
 };
 
-
 export const updateTask = async (formData) => {
-    const id = formData.get("id");
-    const content = formData.get("content");
-    const completed = formData.get("completed");
+  const id = formData.get("id");
+  const content = formData.get("content");
+  const completed = formData.get("completed");
 
-    await prisma.task.update({
-        where: {
-            id
-          },
-          data: {
-            content,
-            completed: completed === "on" ? true : false
-          }
-    })
-    redirect("/tasks")
-}
+  await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+      completed: completed === "on" ? true : false,
+    },
+  });
+  redirect("/tasks");
+};
